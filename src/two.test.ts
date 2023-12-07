b15c057166fbce8a2549d0b04df98a76116a4fe0
Peter Keens <ptkeens@gmail.com>
2023-12-06 20:21:58 -0800

    wip

--- src/two.test.ts
+++ src/two.test.ts
@@ -161,4 +161,29 @@ describe('Day 2', () => {
             expect(result).toBeFalse()
         })
     })
+
+    describe('test input', () => {
+        const input = `
+            Game 1: 3 blue, 4 red; 1 red, 2 green, 6 blue; 2 green
+            Game 2: 1 blue, 2 green; 3 green, 4 blue, 1 red; 1 green, 1 blue
+            Game 3: 8 green, 6 blue, 20 red; 5 blue, 4 red, 13 green; 5 green, 1 red
+            Game 4: 1 green, 3 red, 6 blue; 3 green, 6 red; 3 green, 15 blue, 14 red
+            Game 5: 6 red, 1 blue, 3 green; 2 blue, 1 red, 2 green
+        `
+
+        const bag = {
+            red: 12,
+            blue: 14,
+            green: 13,
+        }
+
+        const result = input
+            .split('\n')
+            .filter((line) => line.length > 0)
+            .map((line) => parseGame(line.trim()))
+            .filter((game) => isGamePosibleWithBag(bag, game))
+            .reduce((total, game) => (total += game.id), 0)
+
+        expect(result).toBe(8)
+    })
 })
